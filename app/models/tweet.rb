f9c50335c7c3a43c2fb555855f4955394e36c8b3
issei-m-0206 <i.m-0206@docomo.ne.jp>
2020-06-11 11:21:03 +0900

    add_carrierwave

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,6 @@ class Tweet < ApplicationRecord
   has_many :tweet_areas
   has_many :areas, through: :tweet_areas, dependent: :destroy
   belongs_to :user
+
+  mount_uploader :image, ImageUploader
 end
